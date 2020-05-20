2526f6c0084450b0971be38ea4a9093f06a9297b
Guillermo Williamson <gwilliamson@Guillermos-Air.lan>
2020-05-20 02:58:51 -0500

    first formated table

--- start.php
+++ start.php
@@ -7,13 +7,43 @@ $loans->connect();
 $allLoans = $loans->all();
 $loans->disconnect();
 
-var_dump($allLoans);
-
 include("header.php");
 ?>
 
-<div class="content">
-SOME CONTENT GOES HERE
+<div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Middle</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Loan</th>
+            <th scope="col">Value</th>
+            <th scope="col">LTV</th>
+        </tr>
+        </thead>
+        <tbody>
+    <?php
+
+    foreach ($allLoans as $loan) {
+    ?>
+        <tr>
+            <th scope="row">{{ $loan['id']}}</th>
+            <td>{{ $loan['id']}}</td>
+            <td>{{ $loan['first_name']}}</td>
+            <td>{{ $loan['middle']}}</td>
+            <td>{{ $loan['last_name']}}</td>
+            <td>{{ $loan['loan']}}</td>
+            <td>{{ $loan['value']}}</td>
+            <td></td>
+        </tr>
+    <?php
+    }
+
+    ?>
+        </tbody>
+    </table>
 </div>
 
 <?php
