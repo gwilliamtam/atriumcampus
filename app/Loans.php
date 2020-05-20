536a530a094e1a64031133c48796db51ecab50ce
Guillermo Williamson <gwilliamson@Guillermos-Air.lan>
2020-05-20 14:58:34 -0500

    add row implemented

--- app/Loans.php
+++ app/Loans.php
@@ -8,23 +8,35 @@ class Loans
 {
     private $conn;
 
+    public function isLocal()
+    {
+        if ($_SERVER['DOCUMENT_ROOT'] == '/Users/gwilliamson/Projects/atriumcampus') {
+            return true;
+        }
+        return false;
+    }
+
     public function connect()
     {
-        $servername = "dev-db1.cluster-ccyyfxhqobvx.us-east-1.rds.amazonaws.com";
-        $username = "guillermo";
-        $password = "XAtqYbM2ZLQYUU2VzD7qAkAmfrEeVx6a";
-        $dbname = "guillermo_test";
+        if (!$this->isLocal()) {
+            $servername = "dev-db1.cluster-ccyyfxhqobvx.us-east-1.rds.amazonaws.com";
+            $username = "guillermo";
+            $password = "XAtqYbM2ZLQYUU2VzD7qAkAmfrEeVx6a";
+            $dbname = "guillermo_test";
 
-        $this->conn = new mysqli($servername, $username, $password, $dbname);
+            $this->conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
         }
     }
 
     public function disconnect()
     {
-        $this->conn->close();
+        if ($this->conn) {
+            $this->conn->close();
+        }
     }
 
     public function populate()
@@ -44,15 +56,18 @@ class Loans
 
     public function all()
     {
-        $this->connect();
-        $sql = "SELECT id, first_name, middle_initial, last_name, loan, value FROM test limit 10";
-        $result = $this->conn->query($sql);
+        if (!$this->isLocal()) {
+            $this->connect();
+            $sql = "SELECT id, first_name, middle_initial, last_name, loan, value FROM test limit 10";
+            $result = $this->conn->query($sql);
 
-        if ($result->num_rows > 0) {
-            // output data of each row
-            return $result->fetch_all(MYSQLI_ASSOC);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                return $result->fetch_all(MYSQLI_ASSOC);
+            }
+            $this->disconnect();
         }
-        $this->disconnect();
-        return null;
+
+        return [];
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Loans as Loans;
 require_once('app/Loans.php');
 
@@ -10,24 +11,24 @@ include("header.php");
 ?>
 
 <div class="container">
-    <table class="table loans-table">
+    <table class="table table-striped" id="loans-table">
         <thead>
         <tr>
-            <th scope="col">Id</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Middle Initial</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Loan</th>
-            <th scope="col">Value</th>
-            <th scope="col">LTV</th>
-            <th scope="col">Action</th>
+            <th scope="col"  class="text-left">Id</th>
+            <th scope="col"  class="text-left">First Name</th>
+            <th scope="col"  class="text-left">Middle Initial</th>
+            <th scope="col"  class="text-left">Last Name</th>
+            <th scope="col"  class="text-right">Loan</th>
+            <th scope="col"  class="text-right">Value</th>
+            <th scope="col"  class="text-right">LTV %</th>
+            <th scope="col"  class="text-center">Action</th>
         </tr>
         </thead>
         <tbody>
     <?php
     foreach ($allLoans as $key => $loan) {
 	echo <<<HTML
-        <tr class="studen-row" student-id="{$loan['id']}">
+        <tr>
             <td>{$loan['id']}</td>
             <td>{$loan['first_name']}</td>
             <td>{$loan['middle_initial']}</td>
@@ -35,23 +36,24 @@ include("header.php");
             <td>{$loan['loan']}</td>
             <td>{$loan['value']}</td>
             <td></td>
-            <td>
-                <button class="btn btn-primary">Update</button>
+            <td class="text-center">
+                <button class="btn btn-primary update-row">Update</button>
             </td>
         </tr>
 HTML;
     }
     ?>
-    <tr>
-        <td colspan="7"></td>
-        <td>
-            <button class="btn btn-primary add-student">Add Student</button>
-        </td>
-    </tr>
         </tbody>
     </table>
-    <div class="">
-        <button class="btn btn-primary save-changes">Save Changes</button>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+            <button class="btn btn-primary add-student">Add Student</button>
+        </div>
+        <div class="col text-center">
+            <button class="btn btn-primary save-changes">Save Changes</button>
+        </div>
     </div>
 </div>
 
@@ -59,39 +61,39 @@ HTML;
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Enter Student Information</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="form-group">
-                    <input type="text" class="form-control" id="first-name" placeholder="First Name" maxlength="30">
+                    <input type="text" class="form-control" id="first-name" placeholder="Enter First Name" maxlength="30">
                     <small id="first-name-help" class="form-text text-muted">First Name</small>
                 </div>
                 <div class="form-group">
-                    <input type="text" class="form-control" id="middle-initial" placeholder="Middle Initial" maxlength="1">
+                    <input type="text" class="form-control" id="middle-initial" placeholder="Enter Middle Initial" maxlength="1">
                     <small id="middle-initial-help" class="form-text text-muted">Middle Initial</small>
                 </div>
                 <div class="form-group">
-                    <input type="text" class="form-control" id="last-name" placeholder="Last Name" maxlength="30">
+                    <input type="text" class="form-control" id="last-name" placeholder="Enter Last Name" maxlength="30">
                     <small id="last-name-help" class="form-text text-muted">Last Name</small>
                 </div>
                 <div class="form-group">
-                    <input type="number" class="form-control" id="loan" placeholder="Loan" maxlength="13">
+                    <input type="number" class="form-control" id="loan" placeholder="Enter Loan" maxlength="13">
                     <small id="loan-help" class="form-text text-muted">Loan</small>
                 </div>
                 <div class="form-group">
-                    <input type="number" class="form-control" id="value" placeholder="Value" maxlength="13">
+                    <input type="number" class="form-control" id="value" placeholder="Enter Value" maxlength="13">
                     <small id="value-help" class="form-text text-muted">Value</small>
                 </div>
                 <div class="form-group">
-                    <input type="number" class="form-control" id="loan-to-value" placeholder="LTV" readonly>
+                    <input type="text" class="form-control" id="loan-to-value" placeholder="LTV" readonly>
                     <small id="loan-to-value-help" class="form-text text-muted">Loan-To-Value</small>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Add / Update</button>
+                <button type="button" class="btn btn-primary" id="add-update">Add / Update</button>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
             </div>
         </div>
@@ -100,19 +102,88 @@ HTML;
 
 <script>
     $(document).ready(function(){
-        let loans = new Loans();
-        loans.load()
+        load();
     });
 
-</script>
-<script>
-    class Loans
+    function load() {
+        $(".add-student").on("click", function() {
+            $('#editor').modal('show');
+        });
+
+        $("#loan").on('change', function() {
+            updateLTV();
+        });
+
+        $("#value").on('change', function() {
+            updateLTV();
+        });
+
+        $('#add-update').on('click', function() {
+            if (validateAddStudent()) {
+                $('#editor').modal('hide');
+                addRow();
+            };
+        });
+    }
+
+    function addRow()
     {
-        load() {
-            $(".add-student").on("click", function() {
-                $('#editor').modal('show');
-            }) ;
+        let table = document.getElementById('loans-table');
+        let row = table.insertRow(-1);
+
+        let cellId = row.insertCell(0);
+        cellId.innerHTML = '';
+
+        let cellName = row.insertCell(1);
+        cellName.innerHTML = $('#first-name').val();
+
+        let cellMiddle = row.insertCell(2);
+        cellMiddle.innerHTML = $('#middle-initial').val();
+
+        let cellLast = row.insertCell(3);
+        cellLast.innerHTML = $('#last-name').val();
+
+        let cellLoan = row.insertCell(4);
+        cellLoan.innerHTML = $('#loan').val();
+        cellLoan.className = 'text-right';
+
+        let cellValue = row.insertCell(5);
+        cellValue.innerHTML = $('#value').val();
+        cellValue.className = 'text-right';
+
+        let cellLTV = row.insertCell(6);
+        cellLTV.innerHTML = $('#loan-to-value').val();
+        cellLTV.className = 'text-right';
+
+        let cellAction = row.insertCell(7);
+        cellAction.innerHTML = '<button class="btn btn-primary update-row">Update</button>';
+        cellAction.className = 'text-center';
+    }
+
+    function validateAddStudent()
+    {
+        if ($('#first-name').val()
+            && $('#last-name').val()
+            && $('#loan').val()
+            && $('#value').val()
+        ) {
+            return true;
         }
+        return false;
+    }
+
+    function updateLTV() {
+
+        const loan = $('#loan').val();
+        const value = $('#value').val();
+        console.log('updating LTV with ', loan, value);
+        let ltv = 0;
+        if (value && loan) {
+            if (loan != 0) {
+                ltv = (loan / value) * 100;
+            }
+        }
+        $('#loan-to-value').val(ltv.toFixed(2));
     }
 </script>
 
